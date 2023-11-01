0b7e50869acf436e0081c08a51e8412c91ea58ac
Rafael Martinelli <martinelli@puc-rio.br>
2023-11-01 11:44:48 -0300

    Changing Excel structure and renaming files.

--- generator.jl
+++ generator.jl
@@ -1,42 +1,37 @@
 using FacilityLocationProblems
 using XLSX
 
-
 file_path = "excel_files/"
 instances = getPMedianInstances()
 
-function generateSheet(sheet, data)
-    XLSX.rename!(sheet, data.name)
-    sheet["A1"] = "Number of medians (p)"
-    sheet["B1"] = data.medians
-    sheet["A2"] = "Medians capacities"
-    sheet["B2"] = data.capacity
+function generateSheet(sheet, data, name)
+    XLSX.rename!(sheet, name)
+    sheet["E1"] = "Number of medians (p)"
+    sheet["F1"] = data.medians
+    sheet["E2"] = "Medians capacities"
+    sheet["F2"] = data.capacity
 
-    sheet["A4"] = "X"
-    sheet["B4"] = "Y"
-    sheet["C4"] = "Demands"
-    sheet["E4"] = "X | Y"
-    sheet["E3"] = "Costs matrix (distances)"
-    sheet["E5", dim=1] = collect(1:length(data.x))
-    sheet["F4"] = collect(1:length(data.y))
+    sheet["A1"] = "X"
+    sheet["B1"] = "Y"
+    sheet["C1"] = "Demands"
 
-    sheet["A5", dim=1] = data.x
-    sheet["B5", dim=1] = data.y
-    sheet["C5", dim=1]= data.demands
-    sheet["F5"]= data.costs
+    sheet["A2", dim=1] = data.x
+    sheet["B2", dim=1] = data.y
+    sheet["C2", dim=1]= data.demands
 end
 
-for idx in 1:10
-    XLSX.openxlsx(file_path * "group" * string(idx) * ".xlsx", mode="w") do xf
+groups = [ "A", "B", "C", "E", "F", "H", "K", "L", "N", "O" ]
+for i in 1:10
+    file_name = joinpath(file_path, "group" * groups[i] * ".xlsx")
+    XLSX.openxlsx(file_name, mode = "w") do xf
         XLSX.addsheet!(xf)
 
-        data1 = loadPMedianProblem(instances[idx])
-        data2 = loadPMedianProblem(instances[idx+10])
-
+        data1 = loadPMedianProblem(instances[i])
         sheet1 = xf[1]
-        sheet2 = xf[2]
+        generateSheet(sheet1, data1, "Case 1")
 
-        generateSheet(sheet1, data1)
-        generateSheet(sheet2, data2)
+        data2 = loadPMedianProblem(instances[i + 10])
+        sheet2 = xf[2]
+        generateSheet(sheet2, data2, "Case 2")
     end
 end
